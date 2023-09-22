ce58b1d1ca161a5f077babc5907d2c434d030c3a
Suyog Shrestha <suyogshrestha.tech@gmail.com>
2023-09-22 16:34:37 -0400

    [Added] In-class exercise 1

---
+++ Task1/script.js
@@ -0,0 +1,5 @@
+console.log('This should print logs in the console.');
+console.log(
+  'Shortcut to open console/network tab is option + command + i in mac OS.'
+);
+console.log('All the logs should be displayed in the console!');
